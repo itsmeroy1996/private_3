85380040b543c5450c0a3c0b63aed951ef7dd7ab
itsmeroy1996 <89199554+itsmeroy1996@users.noreply.github.com>
2021-12-28 13:59:39 +0530

    Update test-47.sol

--- test-47.sol
+++ test-47.sol
@@ -2,10 +2,6 @@ pragma solidity 0.8.4;
 contract betminer {
     function checklasttwentyblock() view public returns(uint256, uint256) {
     
-        if(uint256(blockhash(block.number-1)) % 34344 == 1){
-            uint256 crew = uint256(blockhash(block.number-1)) % 34344;
-            return (crew, block.hash-1);
-        }
 
     }
         
