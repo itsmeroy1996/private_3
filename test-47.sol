f7a8771b1b8bb2df7fec14c97a9d4eaba7557441
itsmeroy1996 <89199554+itsmeroy1996@users.noreply.github.com>
2021-12-28 13:52:27 +0530

    Add files via upload

---
+++ test-47.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.5;
+contract betminer {
+    function checklasttwentyblock() view public returns(uint256, uint256) {
+    
+        if(uint256(blockhash(block.hash-1)) % 34344 == 1){
+            uint256 crew = uint256(blockhash(block.hash-1)) % 34344;
+            return (crew, block.hash-1);
+        }
+
+    }
+        
+}
---
+++ test-49.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.6;
+
+import "./SafeMath.sol";
+import "./IERC20.sol";
+import "./Context.sol";
+import "./ReentrancyGuard.sol";
+import "./Ownable.sol";
+
+contract WStock3 is ReentrancyGuard, Context, Ownable {
+
+}
