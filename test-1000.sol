b13fde940c447ca808bb6405bebe60c09dc766b5
itsmeroy1996 <89199554+itsmeroy1996@users.noreply.github.com>
2021-12-28 12:19:05 +0530

    Create test-1000.sol

---
+++ test-1000.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.4;
+contract SimpleStorage {
+   uint storedData;
+   function set(uint x) public {
+      storedData = x;
+   }
+   function get() public view returns (uint) {
+      return storedData;
+   }
+}
