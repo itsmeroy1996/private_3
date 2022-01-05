92e0cf51e9516b35a97cb47faf83ef35fe19671b
itsmeroy1996 <89199554+itsmeroy1996@users.noreply.github.com>
2022-01-05 10:07:15 +0530

    Add files via upload

---
+++ test-45.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.4;
+contract SimpleStorage {
+  using SafeMath for uint256;
+
+   uint storedData;
+   function set(uint x) public {
+      storedData = x;
+   }
+   function get() public view returns (uint) {
+      return storedData;
+   }
+
+}
