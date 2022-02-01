3d4fc17f019b95314adf7e5da1f68b96c52fa13c
itsmeroy1996 <89199554+itsmeroy1996@users.noreply.github.com>
2022-02-01 12:12:17 +0530

    Update test-48.sol

--- test-48.sol
+++ test-48.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.4;
+pragma solidity 0.8.6;
 
 import "./SafeMath.sol";
 import "./IERC20.sol";
@@ -11,7 +11,7 @@ import "./Ownable.sol";
 contract WStock3 is ReentrancyGuard, Context, Ownable {
 
   function sell(bytes32 ticker, uint256 amount, uint256 total, uint256 timestamp, uint8 v, bytes32 r, bytes32 s) public nonReentrant {
-    address signer = ecrecover(hash, v, r, s);
+    //address signer = ecrecover(hash, v, r, s);
     require(signer == authAddress, "Invalid signature");
 
   }
