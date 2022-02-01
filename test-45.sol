5630f7cd586cb0a002677ed6e2f1498b26430c67
itsmeroy1996 <89199554+itsmeroy1996@users.noreply.github.com>
2022-02-01 12:04:58 +0530

    Update test-45.sol

--- test-45.sol
+++ test-45.sol
@@ -2,7 +2,7 @@ pragma solidity 0.8.4;
 contract SimpleStorage {
   using SafeMath for uint256;
 
-   //uint storedData;
+   uint storedData;
    function set(uint x) public {
       storedData = x;
    }
